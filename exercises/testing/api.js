bb34bd691effc85aeaa1869d275089d9a1e032e1
Alex Y <probesalesman@gmail.com>
2020-05-22 22:22:55 -0400

    adds solution for debugging the testing module

--- exercises/testing/api.js
+++ exercises/testing/api.js
@@ -1,24 +1,34 @@
-const express = require('express')
-const morgan = require('morgan')
-const { urlencoded, json } = require('body-parser')
-const users = require('./users')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const { urlencoded, json } = require("body-parser");
+const users = require("./users");
+const app = express();
 
-app.use(morgan('dev'))
-app.use(urlencoded({extended: true}))
-app.use(json())
+app.use(morgan("dev"));
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
-app.get('/user/:id', async (req, res) => {
-  const id = req.id
-  // should ge user by given id in route param
-  const user = await users.findUser(user => user.id === id)
-  res.status(200).send(user)
-})
+app.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // should ge user by given id in route param
+    const user = await users.findUser(parseInt(id));
+    res.status(200).send(user);
+  } catch (err) {
+    res.send("there was an error " + err.message);
+  }
+});
 
-app.delete('/user/:id', async (req, res) => {
-  const id = req.id
-  await users.deleteUser(id)
-  res.status(201).send({id})
-})
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
 
-module.exports = app
+    await users.deleteUser(id);
+
+    res.status(201).send({ id });
+  } catch (err) {
+    res.send("there was an error " + err.message);
+  }
+});
+
+module.exports = app;
--- exercises/testing/users.js
+++ exercises/testing/users.js
@@ -1,35 +1,44 @@
-const users = new Array(20).fill(0)
-.map((_, i) => {
+const users = new Array(20).fill(0).map((_, i) => {
   return {
     id: i,
     createdAt: Date.now() + i,
-    email: `readycoder${i}@gmail.com`
-  }
-})
+    email: `readycoder${i}@gmail.com`,
+  };
+});
 
 // simulate async db call with promise
-const findUser = (id) => new Promise((resolve, reject) => {
-  const user = users.find(user => user.id === id)
-  if (user) {
-    return resolve(user)
-  }
-  reject(new Error(`No user with id "${id}"`))
-})
+const findUser = (id) =>
+  new Promise((resolve, reject) => {
+    const userToFind = users.find((user) => {
+      return user.id === id;
+    });
+
+    if (userToFind) {
+      return resolve(userToFind);
+    }
+    reject(new Error(`No user with id "${id}"`));
+  });
 
 // simulate async db call with promise
-const deleteUser = (id) => new Promise((resolve, reject) => {
-  const id = fixId(id)
-  const i = users.findIndex(user => user.id === id)
+const deleteUser = (id) =>
+  new Promise((resolve, reject) => {
+    const i = users.findIndex((user) => user.id == id);
+    console.log(
+      "the index of thing to be deleted",
+      i,
+      "and type of id passed in",
+      typeof id
+    );
+    if (i < 0) {
+      return reject(new Error(`No user with id "${newId}"`));
+    }
 
-  if (i < 0) {
-    return reject(new Error(`No user with id "${id}"`))
-  }
-
-  users.slice(i, 1)
-  resolve({id})
-})
+    users.splice(i, 1);
+    console.log("mutated users array", users);
+    resolve({ id });
+  });
 
 module.exports = {
   findUser,
-  deleteUser
-}
+  deleteUser,
+};
